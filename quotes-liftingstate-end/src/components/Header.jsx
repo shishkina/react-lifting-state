6a5fde0c7c45806958725f240601de13226896b8
shishkina <shishkina.kate@gmail.com>
2017-12-12 10:18:41 -0500

    adds final app

---
+++ quotes-liftingstate-end/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Our Own Quotes App!</h1>
+      <h3> ( In case you are not sick of quotes yet 😁 )</h3>
+    </header>
+  );
+};
+
+export default Header;
